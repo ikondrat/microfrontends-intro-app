6ccadbe0ea4a46ff732348addb6cc513801b4378
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2020-03-19 12:16:27 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory, History } from 'history';
 
-function App() {
+const defaultHistory = createBrowserHistory();
+
+const App = ({ history }: { history?: History }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history || defaultHistory}>
+      <div>Sell application</div>
+    </Router>
   );
-}
+};
 
 export default App;
