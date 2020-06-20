a87987f702a8ccc0a4f30f8ac9e132bf854cd9de
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2020-06-20 15:05:29 +0200

    test commit

--- src/index.tsx
+++ src/index.tsx
@@ -19,9 +19,6 @@ if (window.frontends) {
   window.frontends.intro = {
     register: (containerId, history) => {
       ReactDOM.render(<App history={history} />, document.getElementById(containerId));
-      // If you want your app to work offline and load faster, you can change
-      // unregister() to register() below. Note this comes with some pitfalls.
-      // Learn more about service workers: https://bit.ly/CRA-PWA
       serviceWorker.unregister();
     },
     unregister: (containerId) => {
